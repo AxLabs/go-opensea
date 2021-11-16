62c0c0fee067d87dd51f64a4acbfaa146e9e437a
Guilherme Sperb Machado <guil@axlabs.com>
2021-11-16 19:51:52 +0100

    comment some tests (WIP)

--- order_test.go
+++ order_test.go
@@ -1,20 +1,15 @@
 package opensea
 
-import (
-	"testing"
-	"time"
-)
-
-func TestGetOrders(t *testing.T) {
-	is := initializeTest(t)
-
-	since := time.Now().Unix() - 86400
-	ret, err := o.GetOrders(contract, since)
-	is.Nil(err)
-
-	print(len(ret))
-
-	print(*ret[0])
-	print(ret[0].IsPrivate())
-	print(ret[0].BasePrice.Big())
-}
+//func TestGetOrders(t *testing.T) {
+//	is := initializeTest(t)
+//
+//	since := time.Now().Unix() - 86400
+//	ret, err := o.GetOrders(contract, since)
+//	is.Nil(err)
+//
+//	print(len(ret))
+//
+//	print(*ret[0])
+//	print(ret[0].IsPrivate())
+//	print(ret[0].BasePrice.Big())
+//}
